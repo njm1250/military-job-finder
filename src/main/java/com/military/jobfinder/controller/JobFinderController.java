8851348581cf36965d61f0efb5c2167dca2242cb
njm1250 <njm1250@gmail.com>
2023-07-30 22:54:23 +0900

    Added Controller, Service and implemented API call to fetch [military job].

---
+++ src/main/java/com/military/jobfinder/controller/JobFinderController.java
@@ -0,0 +1,33 @@
+package com.military.jobfinder.controller;
+
+import com.military.jobfinder.service.JobFinderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class JobFinderController {
+
+    private final JobFinderService jobFinderService;
+
+    @Autowired
+    public JobFinderController(JobFinderService jobFinderService) {
+        this.jobFinderService = jobFinderService;
+    }
+
+    @GetMapping("")
+    @ResponseBody
+    public String test() {
+        return "TEST";
+    }
+
+    @GetMapping("/api/getJobData")
+    @ResponseBody
+    public String apiData() throws Exception {
+        String data = jobFinderService.getApiData();
+        return data;
+    }
+
+
+}
---
+++ src/main/java/com/military/jobfinder/service/JobFinderService.java
@@ -0,0 +1,7 @@
+package com.military.jobfinder.service;
+
+import org.springframework.stereotype.Service;
+
+public interface JobFinderService {
+    public String getApiData() throws Exception;
+}
