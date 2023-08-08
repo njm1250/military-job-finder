0bb35f61473c5009b72a863c7543931169cc0128
njm1250 <njm1250@gmail.com>
2023-08-08 21:36:07 +0900

    Refactored API call service, DI(ObjectMapper)

--- src/main/java/com/military/jobfinder/service/impl/JobFinderServiceImpl.java
+++ src/main/java/com/military/jobfinder/service/impl/JobFinderServiceImpl.java
@@ -5,17 +5,14 @@ import org.json.JSONObject;
 import org.json.XML;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.URI;
-import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -23,31 +20,57 @@ import java.util.stream.Collectors;
 @Service
 public class JobFinderServiceImpl implements JobFinderService {
     private static final Logger logger = LoggerFactory.getLogger(JobFinderServiceImpl.class);
-    private final String SERVICE_KEY = "92dpMWrx/kUoW09ic/OWGP9bwEdw3uLKiG59NGvV1ue1m9HTK/xaLURJGDc8qYHgh6A9SmfJHeBPmiAmZIGYyQ==";
+
+    @Value("${api.service.key}")
+    private String SERVICE_KEY;
+
+    @Value("${api.url}")
+    private String apiUrl;
+
+    private final HttpClient client = HttpClient.newHttpClient();
+
+    @Override
     public String getApiData() throws Exception {
-        HttpClient client = HttpClient.newHttpClient();
+        int numOfRows = 10;
+        int pageNo = 1;
+        URI uri = buildUri(numOfRows, pageNo);
+        HttpRequest request = createHttpRequest(uri);
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() >= 200 && response.statusCode() <= 300){
+            return convertXmlToJson(response.body());
+        } else {
+            throw new Exception("Unexpected response status : " + response.statusCode());
+            //TODO: 2023-08-08  Need to add more error handling method
+        }
+    }
+
+    private URI buildUri(int numOfRows, int pageNo) throws Exception {
         Map<String, String> parameters = Map.of(
                 "serviceKey", SERVICE_KEY,
-                "numOfRows", "10",
-                "pageNo", "1"
+                "numOfRows", String.valueOf(numOfRows),
+                "pageNo", String.valueOf(pageNo)
         );
+
         String params = parameters.entrySet().stream()
                 .map(p -> URLEncoder.encode(p.getKey(), StandardCharsets.UTF_8) + "=" + URLEncoder.encode(p.getValue(), StandardCharsets.UTF_8))
                 .collect(Collectors.joining("&"));
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://apis.data.go.kr/1300000/CyJeongBo/list?" + params))
+        return URI.create(apiUrl + "?" + params);
+    }
+
+    private HttpRequest createHttpRequest(URI uri) {
+        return HttpRequest.newBuilder()
+                .uri(uri)
                 .header("Content-Type", "application/xml")
                 .GET()
                 .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        if (response.statusCode() >= 200 && response.statusCode() <= 300){
-            String xmlResponse = response.body();
-            JSONObject jsonResponse = XML.toJSONObject(xmlResponse); // XML to JSON
-            logger.info("API call completed successfully.");
-            return jsonResponse.toString();
-        } else {
-            throw new Exception("Unexpected response status : " + response.statusCode());
-        }
+    }
+
+    private String convertXmlToJson(String xmlResponse) {
+        JSONObject jsonResponse = XML.toJSONObject(xmlResponse); // XML to JSON
+        logger.info("API call completed successfully.");
+        return jsonResponse.toString();
     }
 }
